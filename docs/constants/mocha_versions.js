27b2546490b96741812547af91a320429ff35e70
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-09-22 17:09:53 -0400

    reset to https://github.com/lazyledger/devrels-docs/commit/fbabb9222991c74f935cd7e8a68b1ea316cb3927

---
+++ docs/constants/mocha_versions.js
@@ -0,0 +1,9 @@
+const mochaVersions = Object.freeze({
+  "app-latest-tag": "v1.0.0-rc16",
+  "app-latest-sha": "44e13fad9a24f858dc2707ed3bc9f07818785206",
+  "core-latest-tag": "v1.27.0-tm-v0.34.28",
+  "core-latest-sha": "074d98180f2e71ea0213e4a10ad4b0c460f995da",
+  "node-latest-tag": "v0.11.0-rc13",
+  "node-latest-sha": "8ca777e1a3f04b7d0494e3bae8a1c1bcf68c5d8c",
+});
+export default mochaVersions;
